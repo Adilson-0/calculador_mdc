27b06ac167ea024510c9ddc6c5bb0abd4cf6e6e7
Adilson Fernandes da Silva Filho <20221780038@ad.ifpb.edu.br>
2022-09-06 09:50:32 -0300

    criado site para calcular máximo divisor comum de um número.

---
+++ app.js
@@ -0,0 +1,32 @@
+var input01 = document.getElementById("num1");
+var input02 = document.getElementById("num2");
+const button = document.getElementById("calc");
+var c = [];
+
+button.addEventListener('click', function(){
+    let c =[];
+    for(let i = 0; i <= Number(input01.value); i++){
+        if(Number(input01.value) % i == 0 ){
+            c.push(i);
+        }
+    }
+    for(let i = 0; i <= Number(input02.value); i++){
+        if(Number(input02.value) % i == 0 ){
+            c.push(i);
+        }
+    }
+
+    var result = c.filter(function(element, i){
+        return c.indexOf(element) != i;
+    });
+    result.sort(function(a, b){
+        if(a > b) return 1;
+        if( a < b) return -1;
+        return 0;
+    })
+    result.splice(0, (result.length - 1));
+    alert(result);
+    
+});
+
+
